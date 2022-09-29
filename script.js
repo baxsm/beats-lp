a858cd663e3bc337015c1236a24c986ba7e2884d
Bobby Aleem <78314667+baxsm@users.noreply.github.com>
2022-09-29 22:59:37 +0500

    update

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
-if(navToggle) {
+if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu');
     })
 }
 
-if(navClose) {
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
     })
@@ -24,7 +24,7 @@ navLink.forEach(x => x.addEventListener('click', linkAction));
 
 function scrollHeader() {
     const header = document.getElementById('header');
-    if(this.scrollY >= 100) {
+    if (this.scrollY >= 100) {
         header.classList.add('scroll-header');
     }
     else {
@@ -33,3 +33,49 @@ function scrollHeader() {
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 200) {
+        scrollUp.classList.add('show-scroll');
+    }
+    else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        var sectionId = current.getAttribute('id');
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }
+        else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+});
+
+sr.reveal('.home__header, .section__title', { delay: 600 });
+sr.reveal('.home__footer', { delay: 700 });
+sr.reveal('.home__img', { delay: 900, origin: 'top' });
+sr.reveal('.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy', { origin: 'top', interval: 100 });
+sr.reveal('.specs__data, .discount__animate', { origin: 'left', interval: 100 });
+sr.reveal('.specs__img, .discount__img', { origin: 'right' });
+sr.reveal('.case__img', { origin: 'top' });
+sr.reveal('.case__data');
